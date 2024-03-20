633997cff71c84e85b0b9ae99fd9e8d1c3f0319c
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-03-20 09:49:29 +0000

    Added project layout

---
+++ nike/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+
+
+const Nav = () => {
+  return (
+    <header>
+        <nav>
+            <a href="/">
+                <img src="" alt="Logo"/>
+            </a>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
---
+++ nike/src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+
+export {
+    Hero,
+    PopularProduct,
+    Services,
+    SuperQuality,
+    Subscribe,
+    SpecialOffer,
+    Footer,
+    CustomerReviews
+}
